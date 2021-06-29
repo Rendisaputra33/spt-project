66809162efec0b401830e00f80e0c62d5d71e038
Ferdino Subastian <ferdinosubastian35223@gmail.com>
2021-06-29 09:48:34 +0700

    update coba invoice

--- resources/views/coba-invoice.blade.php
+++ resources/views/coba-invoice.blade.php
@@ -13,6 +13,11 @@
 
     <!-- Latest compiled and minified JavaScript -->
     <script src="https://stackpath.bootstrapcdn.com/bootstrap/3.4.1/js/bootstrap.min.js" integrity="sha384-aJ21OjlMXNL5UyIl/XNwTMqvzeRMZH2w8c5cRVpzpU8Y5bApTppSuUkhZXN0VxHd" crossorigin="anonymous"></script>
+    <style>
+        hr {
+           border: 1px solid black; 
+        }
+    </style>
 </head>
 
 <body>
