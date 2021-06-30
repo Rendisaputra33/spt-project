7a58a15ad04e16b4e39a7c677270f8963c38c2c2
dnothingnes <rendisaputraofficial@gmail.com>
2021-06-30 16:22:58 +0700

    update

--- public/Js/LaporanPembayaran.js
+++ public/Js/LaporanPembayaran.js
@@ -1,46 +1,50 @@
-const URL = document.getElementById("url").value;
-const TOKEN = document.getElementById("token").value;
+const URL = document.getElementById('url').value;
+const TOKEN = document.getElementById('token').value;
 
 document.getElementById('filter').addEventListener('click', function () {
-    filter();
-})
+	filter();
+});
 
 function filter() {
-    try {
-        getfilter();
-    } catch (e) {
-        console.log(e);
-        document.getElementById("list-data").innerHTML = "error";
-    }
+	try {
+		getfilter();
+	} catch (e) {
+		console.log(e);
+		document.getElementById('list-data').innerHTML = 'error';
+	}
 }
 
 function getfilter() {
-    const pabrik = document.getElementById('pabrik').value;
-    const type = document.getElementById('type').value;
-    const date = document.getElementById('date-range').value;
-    const split = date.split(' / ');
-    fetch(`${URL}/filterlaporan`, {
-        method: "post",
-        body: JSON.stringify({ tgl1: split[0], tgl2: split[1], type: type, tujuan: pabrik }),
-        headers: { "Content-Type": "application/json", "X-CSRF-Token": TOKEN },
-    })
-        .then((res) => res.json())
-        .then((res) => {
-            document.getElementById("list-data").innerHTML = parse(res);
-        });
-
+	const pabrik = document.getElementById('pabrik').value;
+	const type = document.getElementById('type').value;
+	const date = document.getElementById('date-range').value;
+	const split = date.split(' / ');
+	fetch(`${URL}/filterlaporan`, {
+		method: 'post',
+		body: JSON.stringify({
+			tgl1: split[0],
+			tgl2: split[1],
+			type: type,
+			tujuan: pabrik,
+		}),
+		headers: { 'Content-Type': 'application/json', 'X-CSRF-Token': TOKEN },
+	})
+		.then(res => res.json())
+		.then(res => {
+			document.getElementById('list-data').innerHTML = parse(res);
+		});
 }
 
-const parse = (data) => {
-    let html = "";
-    data.data.map((res) => {
-        html += htmldata(res);
-    });
-    return html;
-}
+const parse = data => {
+	let html = '';
+	data.data.map(res => {
+		html += htmldata(res);
+	});
+	return html;
+};
 
-const htmldata = (res) => {
-    return /*html*/ ` <tr>
+const htmldata = res => {
+	return /*html*/ ` <tr>
     <td>${res.tipe}</td>
     <td>${res.no_invoice}</td>
     <td>${res.harga}</td>
@@ -49,5 +53,5 @@ const htmldata = (res) => {
     <td>${res.netto}</td>
     <td>${res.pabrik_tujuan}</td>
     <td><a href="/transaksi/pembayaran/cetak/${res.id_pembayaran}" class="btn btn-success">cetak</a></td>
-</tr>`
-}
+</tr>`;
+};
